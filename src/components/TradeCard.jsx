ebe72f9b3d67394156b58480441e2bb53d821390
enochdev2 <enochthechristepistle@gmail.com>
2025-05-26 13:19:53 +0100

    adding update

--- src/components/TradeCard.jsx
+++ src/components/TradeCard.jsx
@@ -17,50 +17,51 @@ const TradeCard = ({ offer, sell }) => {
 
   return (
     <div
-      className={`flex flex-col sm:flex-row items-center bg-white rounded-lg p-4 mb-4 border border-gray-200 shadow-sm
+      className={`flex flex-co sm:flex-row items-center bg-white rounded-lg py-2 px-2 sm:py-3 md:py-4 mb-4 border border-gray-200 shadow-sm
         ${isPending ? "opacity-90 filter grayscale" : ""}
       `}
     >
       {/* Left Section */}
-      <div className="flex items-center w-full sm:w-24 mb-4 sm:mb-0 sm:mr-6 justify-center sm:justify-start">
-        <div className="flex items-center space-x-2">
-          <div className="bg-green-600 text-white font-semibold text-base sm:text-sm md:text-base px-4 py-2 rounded-md select-none whitespace-nowrap">
+      <div className="flex items-center sm:w-f sm:w-24 mb-4 sm:mb-0 sm:mr-6 justify-center sm:justify-start">
+        <div className="flex items-center space-x-1 md:space-x-2">
+          <div className="bg-green-600 text-white font-semibold text-xs sm:text-sm md:text-base px-1 md:px-4 py-2 rounded-md select-none whitespace-nowrap">
             {sell ? "Sell" : "Buy"}
           </div>
-          <div className="w-3 h-3 bg-green-700 rounded-full mt-1" />
+          <div className="w-3 h-3 hidden sm:block bg-green-700 rounded-full mt-1" />
         </div>
       </div>
 
       {/* Center Left Section */}
-      <div className="flex-1 w-full sm:w-auto flex items-center mb-4 sm:mb-0">
-        <img src={logo2} alt="Tether logo" className="w-7 h-7 mr-3" />
-        <span className="font-semibold text-lg sm:text-xl text-gray-900 truncate">
+      <div className="flex-col md:flex-row md:flex-1 w-full sm:w-auto flex items-center mb-4 sm:mb-0">
+        <img src={logo2} alt="Tether logo" className="w-5 h-5 md:w-7 md:h-7 mr-3" />
+        <span className="font-medium text-xs sm:text-xl text-gray-900 truncate">
           {offer.amount} USDT
         </span>
       </div>
 
       {/* Center Right Section */}
-      <div className="flex-1 w-full sm:w-auto font-semibold text-gray-900 text-base sm:text-lg mb-4 sm:mb-0 text-center sm:text-left truncate">
+      <div className="flex-1 w-full sm:w-auto font-semibold text-gray-900 text-xs sm:text-lg mb-4 sm:mb-0 text-center sm:text-left truncat">
         {offer.krwAmount
           ? `₩${offer.krwAmount.toLocaleString()} KRW`
           : `₩${offer.price.toLocaleString()} KRW`}
       </div>
 
       {/* Right Section */}
-      <div className="flex flex-col flex-1 w-full sm:w-32 items-center sm:items-end text-gray-800 text-sm space-y-1">
+      <div className="flex flex-col flex-wrap sm:flex-nowrap flex-1 w-full sm:w-32 items-center sm:items-end text-gray-800 text-xs space-y-1">
         <div className="break-words text-center sm:text-right w-full sm:w-auto truncate">
           {offer._id.slice(0, 10)}
         </div>
         <div className="flex items-center space-x-2">
           <div
-            className="w-3 h-3 rounded-full flex-shrink-0"
+            className="w-3 h-3 hidden sm:block rounded-full flex-shrink-0"
             style={{ backgroundColor: statusColors[offer.status] || "#ccc" }}
           />
           <span
             className={`font-semibold select-none truncate max-w-[8rem] ${
               offer.status === "Pending Approval"
                 ? "text-gray-400"
-                : offer.status === "Sell completed" || offer.status === "Buy Completed"
+                : offer.status === "Sell completed" ||
+                  offer.status === "Buy Completed"
                 ? "text-orange-500"
                 : "text-lime-600"
             }`}
