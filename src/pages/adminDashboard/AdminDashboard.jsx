00e85b9420295b172d1d812bd80fec209f667aca
Enoch Promise Akhabue <enochpromise.va@gmail.com>
2025-06-13 12:03:36 +0100

    adding more functionalities

--- src/pages/adminDashboard/AdminDashboard.jsx
+++ src/pages/adminDashboard/AdminDashboard.jsx
@@ -6,8 +6,10 @@ import InfoCard from "../../components/InfoCard";
 import { SettingsIcon, User2 } from "lucide-react";
 import UsersCard from "../../components/UsersCard";
 import DashboardMetrics from "../../components/DashboardMetrics";
+import { useTranslation } from "react-i18next";
 
 const AdminDashboard = () => {
+  const { t } = useTranslation();
   const { user, setIsLoggedIn, setUser } = useAuth();
   const navigate = useNavigate();
   const [stats, setStats] = useState({
@@ -44,24 +46,23 @@ const AdminDashboard = () => {
   }, []);
 
   return (
-   <div className="flex mt-19 min-h-screen bg-gray-100 overflow-x-auto">
-  {/* Sidebar Section */}
-  <div className=" bg-white shadow-md">
-    <Sidebar />
-  </div>
+    <div className="flex mt-19 min-h-screen bg-gray-100 overflow-x-auto">
+      {/* Sidebar Section */}
+      <div className=" bg-white shadow-md">
+        <Sidebar />
+      </div>
 
-  {/* Main Content Area */}
-  <div className="flex-1  p-4 sm:p-6 lg:p-8 space-y-4 overflow-y-auto">
-    {/* Header */}
-    <div className="border-b flex items-center px-2 sm:px-3 py-3 border-slate-300 shadow-sm">
-      <h1 className="text-2xl sm:text-3xl font-bold">Admin Dashboard</h1>
+      {/* Main Content Area */}
+      <div className="flex-1  p-4 sm:p-6 lg:p-8 space-y-4 overflow-y-auto">
+        {/* Header */}
+        <div className="border-b flex items-center px-2 sm:px-3 py-3 border-slate-300 shadow-sm">
+          <h1 className="text-2xl sm:text-3xl font-bold">{t("AdminDashboard")}</h1>
+        </div>
+
+        {/* Dynamic Content Rendering */}
+        <Outlet />
+      </div>
     </div>
-
-    {/* Dynamic Content Rendering */}
-    <Outlet />
-  </div>
-</div>
-
   );
 };
 
