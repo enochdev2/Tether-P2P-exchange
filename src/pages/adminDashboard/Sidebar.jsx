54a36fcb3da3a296268b3f6b387042314ae3e52f
Enoch Promise Akhabue <enochpromise.va@gmail.com>
2025-06-13 08:24:24 +0100

    adding more functionalities

--- src/pages/adminDashboard/Sidebar.jsx
+++ src/pages/adminDashboard/Sidebar.jsx
@@ -6,6 +6,7 @@ import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 import { HiOutlineSwitchHorizontal } from "react-icons/hi";
 import { BiChat } from "react-icons/bi";
 import { FaUserCog } from "react-icons/fa";
+import { useTranslation } from "react-i18next";
 import { t } from "i18next";
 
 const sidebarSections = [
@@ -37,6 +38,7 @@ const sidebarSections2 = [
 ];
 
 export default function AdminSidebar() {
+   const { t } = useTranslation();
   const [collapsed, setCollapsed] = useState(false);
 
   const navigate = useNavigate();
@@ -150,17 +152,180 @@ export default function AdminSidebar() {
   );
 }
 
-// const Item = ({ to, children }) => (
-//   <li>
-//     <Link
-//       to={to}
-//       className={({ isActive }) =>
-//         `block text-white px-4 py-2 rounded transition ${
-//           isActive ? "bg-[#26a17b]" : "hover:bg-gray-700"
-//         }`
-//       }
+
+// const sidebarSections = [
+//   {
+//     title: "General",
+//     items: [
+//       { label: "Dashboard", to: "/admin/dashboard", icon: <User /> },
+//       { label: "Transactions", to: "/admin/transactions", icon: <HiOutlineSwitchHorizontal /> },
+//     ],
+//   },
+//   {
+//     title: "Live",
+//     items: [
+//       {
+//         label: "Sell Orders (Live)",
+//         to: "/admin/sell-orders",
+//         icon: <TrendingUp />,
+//       },
+//       {
+//         label: "Buy Orders (Live)",
+//         to: "/admin/buy-orders",
+//         icon: <TrendingUp />,
+//       },
+//       {
+//         label: "Sell/Buy 1:1 Chat (Live)",
+//         to: "/admin/chat",
+//         icon: <BiChat />,
+//       },
+//     ],
+//   },
+//   // {
+//   //   title: "User",
+//   //   items: [{ label: "User Management", to: "/admin/users", icon: <FaUserCog /> }],
+//   // },
+//   // {
+//   //   title: "Inquiry",
+//   //   items: [{ label: "1:1 Inquiry Box (Live)", to: "/admin/inquiries", icon: <HelpCircle />, }],
+//   // },
+// ];
+// const sidebarSections2 = [
+//   {
+//     title: "User",
+//     items: [{ label: "User Management", to: "/admin/users", icon: <FaUserCog /> }],
+//   },
+//   {
+//     title: "Inquiry",
+//     items: [{ label: "1:1 Inquiry Box (Live)", to: "/admin/inquiries", icon: <HelpCircle />, }],
+//   },
+// ];
+
+// export default function AdminSidebar() {
+//   const [collapsed, setCollapsed] = useState(false);
+
+//   const navigate = useNavigate();
+//   const { logout } = useAuth();
+//   const location = useLocation();
+
+//   const isActive = (path) => location.pathname === path;
+
+//   const handleLogout = async () => {
+//     const res = await logout();
+//     if(res){
+//       navigate("/");
+//     }
+//   };
+
+//   const Section = ({ title, children }) => (
+//     <div className="mt-6 px-4">
+//       <div className="flex gap-2 items-center mb-2">
+//           <h3 className={` uppercase ${collapsed && title ? 'text-[10px] font-[500] text-shadow-white' : 'text-gray-400 text-xs font-semibold'}`}>{title}</h3>
+//       <div className={`h-px bg-blue-500 w-[60%] my-2 ${collapsed ? 'hidden' : 'block'}`}></div>
+//       </div>
+//       <ul className="space-y-1">{children}</ul>
+//     </div>
+//   );
+
+//   const Item = ({ to, children }) => (
+//     <li>
+//       <Link
+//         to={to}
+//         className={`flex items-center gap-3 py-2 px-3 rounded-md transition-colors ${
+//           isActive(to)
+//             ? "bg-[#26a17b] text-white"
+//             : "hover:bg-gray-700 text-gray-300"
+//         }`}
+//       >
+//         {children}
+//       </Link>
+//     </li>
+//   );
+
+//   return (
+//     <aside
+//       className={` z-10 pt-4  bg-[#1f2937] text-white transition-all duration-300 ${
+//         collapsed ? "w-[75px]" : "lg:relative fixed w-[300px]"
+//       } h-full`}
 //     >
-//       {children}
-//     </Link>
-//   </li>
-// );
+//       {/* header */}
+//       <div className="flex items-center justify-between p-4">
+//         {!collapsed && (
+//           <div className="flex items-center space-x-4">
+//             <div className="rounded-full bg-[#26a17b flex items-center pl- justify-center text-white font-bold text-lg space-x-3 md:space-x-6">
+              
+//               <h3 className="hidden sm:block p-3 rounded-full bg-[#26a17b] font-bold">
+//                 AD
+//               </h3>
+//               <span className="ml-3 font-medium text-base">Admin Panel</span>
+//             </div>
+//           </div>
+//         )}
+
+//         <button
+//           className="cursor-pointer lg:hidden"
+//           onClick={() => setCollapsed(!collapsed)}
+//         >
+//           {/* {collapsed ? <Menu size={24} /> : <X size={24} />} */}
+
+//           {collapsed ? <FiArrowRight size={25} /> : <FiArrowLeft size={25} />}
+//         </button>
+//       </div>
+
+//       <div className="">
+//         {sidebarSections.map((section) => (
+//           <Section key={section.title} title={section.title}>
+//             <ul className="space-y-1">
+//               {section.items.map((item) => (
+//                 <Item key={item.to} to={item.to}>
+//                   {item.icon} 
+//                   {!collapsed && <span>{item.label}</span>}
+//                 </Item>
+//               ))}
+//             </ul>
+//           </Section>
+//         ))}
+//       </div>
+//       <div className="mt-20">
+//         {sidebarSections2.map((section) => (
+//           <Section key={section.title} title={section.title}>
+//             <ul className="space-y-1">
+//               {section.items.map((item) => (
+//                 <Item key={item.to} to={item.to}>
+//                   {item.icon} 
+//                   {!collapsed && <span>{item.label}</span>}
+//                 </Item>
+//               ))}
+//             </ul>
+//           </Section>
+//         ))}
+//       </div>
+
+//       {/* spacer + logout */}
+//       <div className="mt-aut px-4 mt-8 pb-6">
+//         <button
+//           onClick={handleLogout}
+//           className="w-full flex items-center cursor-pointer gap-2 px-4 py-2 rounded hover:bg-red-600 transition"
+//         >
+//           <LogOutIcon size={16} />
+//          {!collapsed &&  <span className="text-white">Log out</span>}
+//         </button>
+//       </div>
+//     </aside>
+//   );
+// }
+
+// // const Item = ({ to, children }) => (
+// //   <li>
+// //     <Link
+// //       to={to}
+// //       className={({ isActive }) =>
+// //         `block text-white px-4 py-2 rounded transition ${
+// //           isActive ? "bg-[#26a17b]" : "hover:bg-gray-700"
+// //         }`
+// //       }
+// //     >
+// //       {children}
+// //     </Link>
+// //   </li>
+// // );
