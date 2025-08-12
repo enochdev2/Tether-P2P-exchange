d20496c16ffa7b062a4a06a671cef39bb70c73ea
enochdev2 <enochpromise.va@gmail.com>
2025-08-12 06:37:24 +0100

    adding functionality

--- src/pages/managerDashboard/Dashboard.jsx
+++ src/pages/managerDashboard/Dashboard.jsx
@@ -23,7 +23,7 @@ const DashboardManager = () => {
       const token = localStorage.getItem("token");
 
       const response = await fetch(
-        "https://tether-p2p-exchang-backend.onrender.com/api/v1/sell/admin/getallstats",
+        "https://tether-p2p-exchang-backend.onrender.com/api/v1/sell/manager/getallstats",
         {
           method: "GET",
           headers: {
