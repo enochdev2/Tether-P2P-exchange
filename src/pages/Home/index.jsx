92ee7896bc95f49b37ac6304a65c91cece9eb184
enochdev2 <enochpromise.va@gmail.com>
2026-02-18 14:12:37 +0100

    I worked on the login page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="relative">
       {/* Absolute Moving Notice */}
-       <div className="absolute top-0 left-0 w-full bg-[#110101] text-red-400 overflow-hidden z-10">
+       <div className="absolute top-20 sm:top-5 md:top-0 left-0 w-full bg-[#110101] text-red-400 overflow-hidden z-10">
         <div className="flex whitespace-nowrap">
           <div className="marquee py-2 text-base font-bold">
             ⚠️ This business is currently on hold. The website is downgraded to a free server and may take up to 50 seconds to load initially.
